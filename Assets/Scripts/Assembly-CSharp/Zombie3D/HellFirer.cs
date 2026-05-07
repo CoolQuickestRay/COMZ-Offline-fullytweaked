f1245be403c50756f2ff3b4aae5f31417b267387
CoolQuickestRay <e6099956354891@gmail.com>
2026-05-06 21:18:22 -0400

    Update HellFirer.cs

--- Assets/Scripts/Assembly-CSharp/Zombie3D/HellFirer.cs
+++ Assets/Scripts/Assembly-CSharp/Zombie3D/HellFirer.cs
@@ -2,81 +2,108 @@ using UnityEngine;
 
 namespace Zombie3D
 {
-	public class HellFirer : Enemy
-	{
-		protected Vector3 targetPosition;
+    public class HellFirer : Enemy
+    {
+        protected Vector3 targetPosition;
+        protected Vector3[] p = new Vector3[4];
+        protected GameObject Hell_Fire;
 
-		protected Vector3[] p = new Vector3[4];
+        // UPDATED: Changed from ParticleEmitter to ParticleSystem
+        protected ParticleSystem FireDream;
+        protected ParticleSystem FireHeart1;
+        protected ParticleSystem FireHeart2;
 
-		protected GameObject Hell_Fire;
+        public override void Init(GameObject gObject)
+        {
+            m_tip_height = 2f;
+            base.Init(gObject);
+            lastTarget = Vector3.zero;
+            runAnimationName = "Forward01";
+            mAttributes.attackRange = 7f;
+            ComputeAttributes(gConfig.GetMonsterConfig("HellFirer"));
+            
+            if (base.IsElite)
+            {
+                mAttributes.MoveSpeed += 2f;
+                animation[runAnimationName].speed = 1.5f;
+            }
 
-		protected ParticleEmitter FireDream;
+            Hell_Fire = enemyTransform.Find("Bip01/Bip01 Pelvis/Bip01 Spine/Bip01 Spine1/Bip01 Neck/Bip01 R Clavicle/Bip01 R UpperArm/Bip01 R Forearm/Bip01 R Hand/Weapon_Dummy/FireGun").gameObject;
+            
+            GameObject gameObject = Hell_Fire.transform.Find("gun_fire_new/hellfire/hellfire_01").gameObject;
+            gameObject.GetComponent<HellFireEnemyScript>().damage = mAttributes.AttackDamage;
+            
+            // UPDATED: Using ParticleSystem component
+            FireDream = gameObject.GetComponent<ParticleSystem>();
+            
+            gameObject = Hell_Fire.transform.Find("gun_fire_new/hellfire/hellfire_02").gameObject;
+            FireHeart1 = gameObject.GetComponent<ParticleSystem>();
+            
+            gameObject = Hell_Fire.transform.Find("gun_fire_new/hellfire/hellfire_03").gameObject;
+            FireHeart2 = gameObject.GetComponent<ParticleSystem>();
+        }
 
-		protected ParticleEmitter FireHeart1;
+        public override void OnAttack()
+        {
+            base.OnAttack();
+            Animate("Fire01", WrapMode.ClampForever);
+            attacked = false;
+            lastAttackTime = Time.time;
+            Fire();
+        }
 
-		protected ParticleEmitter FireHeart2;
+        public override void DoMove(float deltaTime)
+        {
+            enemyTransform.Translate(moveDirection * mAttributes.MoveSpeed * deltaTime, Space.World);
+            StopFire();
+        }
 
-		public override void Init(GameObject gObject)
-		{
-			m_tip_height = 2f;
-			base.Init(gObject);
-			lastTarget = Vector3.zero;
-			runAnimationName = "Forward01";
-			mAttributes.attackRange = 7f;
-			ComputeAttributes(gConfig.GetMonsterConfig("HellFirer"));
-			if (base.IsElite)
-			{
-				mAttributes.MoveSpeed += 2f;
-				animation[runAnimationName].speed = 1.5f;
-			}
-			Hell_Fire = enemyTransform.Find("Bip01/Bip01 Pelvis/Bip01 Spine/Bip01 Spine1/Bip01 Neck/Bip01 R Clavicle/Bip01 R UpperArm/Bip01 R Forearm/Bip01 R Hand/Weapon_Dummy/FireGun").gameObject;
-			GameObject gameObject = Hell_Fire.transform.Find("gun_fire_new/hellfire/hellfire_01").gameObject;
-			gameObject.GetComponent<HellFireEnemyScript>().damage = mAttributes.AttackDamage;
-			FireDream = gameObject.GetComponent<ParticleEmitter>();
-			gameObject = Hell_Fire.transform.Find("gun_fire_new/hellfire/hellfire_02").gameObject;
-			FireHeart1 = gameObject.GetComponent<ParticleEmitter>();
-			gameObject = Hell_Fire.transform.Find("gun_fire_new/hellfire/hellfire_03").gameObject;
-			FireHeart2 = gameObject.GetComponent<ParticleEmitter>();
-		}
+        public void Fire()
+        {
+            enemyTransform.LookAt(player.GetTransform());
+            // UPDATED: Changed .emit = true to .Play()
+            if (FireDream != null) FireDream.Play();
+            if (FireHeart1 != null) FireHeart1.Play();
+            if (FireHeart2 != null) FireHeart2.Play();
+        }
 
-		public override void OnAttack()
-		{
-			base.OnAttack();
-			Animate("Fire01", WrapMode.ClampForever);
-			attacked = false;
-			lastAttackTime = Time.time;
-			Fire();
-		}
+        public void StopFire()
+        {
+            // UPDATED: Changed .emit = false to .Stop()
+            if (FireDream != null) FireDream.Stop();
+            if (FireHeart1 != null) FireHeart1.Stop();
+            if (FireHeart2 != null) FireHeart2.Stop();
+        }
 
-		public override void DoMove(float deltaTime)
-		{
-			enemyTransform.Translate(moveDirection * mAttributes.MoveSpeed * deltaTime, Space.World);
-			StopFire();
-		}
+        public override void Animate(string animationName, WrapMode wrapMode)
+        {
+            if (animationName == "Damage")
+            {
+                animationName = "Damage01";
+            }
+            animation[animationName].wrapMode = wrapMode;
+            if (!animation.IsPlaying("Damage01"))
+            {
+                if (wrapMode == WrapMode.Loop || (!animation.IsPlaying(animationName) && animationName != "Damage01"))
+                {
+                    animation.CrossFade(animationName);
+                    return;
+                }
+                animation.Stop();
+                animation.Play(animationName);
+            }
+        }
 
-		public void Fire()
-		{
-			enemyTransform.LookAt(player.GetTransform());
-			FireDream.emit = true;
-			FireHeart1.emit = true;
-			FireHeart2.emit = true;
-		}
-
-		public void StopFire()
-		{
-			FireDream.emit = false;
-			FireHeart1.emit = false;
-			FireHeart2.emit = false;
-		}
-
-		public override void Animate(string animationName, WrapMode wrapMode)
-		{
-			if (animationName == "Damage")
-			{
-				animationName = "Damage01";
-			}
-			animation[animationName].wrapMode = wrapMode;
-			if (!animation.IsPlaying("Damage01"))
+        public override bool AttackAnimationEnds()
+        {
+            if (Time.time - lastAttackTime > animation["Fire01"].clip.length)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
 			{
 				if (wrapMode == WrapMode.Loop || (!animation.IsPlaying(animationName) && animationName != "Damage01"))
 				{
